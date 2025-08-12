2fa0d634021420d0d9b153e03ce2f69648c565ed
samuel <samawyck@gmail.com>
2025-08-12 12:45:55 -0500

    Create footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+import "../styles/footer.css";
+
+
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; Waldo Wrangler</p>
+        </footer>
+    );
+};
+
+
+
+export default Footer;

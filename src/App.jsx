eed61f8af8ee350721db49f5f3f0e62fce628f01
samuel <samawyck@gmail.com>
2025-08-12 10:19:13 -0500

    Hack out app page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
+import Header from "./components/header.jsx";
+import { Outlet } from 'react-router-dom';
 
 
 
 function App() {
     return (
         <>
-        
+        <Header />
+        <Outlet />
         </>
     );
 };

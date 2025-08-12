ac76ccc80699e7cde9f35c69c66cfcfab8e36420
samuel <samawyck@gmail.com>
2025-08-12 16:41:16 -0500

    Hack out gamePage

---
+++ src/components/gamePage.jsx
@@ -0,0 +1,51 @@
+import "../styles/gamePage.css";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import logic from "../utils/gameLogic.js";
+
+
+
+function GamePage() {
+    const {imageName} = useParams();
+
+    useEffect(function() {
+        document.addEventListener(
+            "click", logic.positionTargetBox
+        );
+
+        return function() {
+            document.removeEventListener(
+                "click", logic.positionTargetBox
+            );
+        };
+    }, []);
+
+
+    return (
+        <main>
+            <div className="game-image-wrapper">
+                <div className="target-box hidden">
+                    <div className="target-btns">
+                        <button 
+                            onClick={logic.makeGuess}
+                        >Waldo</button>
+                        <button 
+                            onClick={logic.makeGuess}
+                        >Wilma</button>
+                        <button 
+                            onClick={logic.makeGuess}
+                        >Wizard</button>
+                        <button 
+                            onClick={logic.hideTargetBox}
+                        >Cancel</button>
+                    </div>
+                </div>
+                <div className="game-image"></div>
+            </div>
+        </main>
+    );
+};
+
+
+
+export default GamePage;

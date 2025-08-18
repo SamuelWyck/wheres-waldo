415b63f11af17f6ebf2a36684c9d3212e3a716ef
samuel <samawyck@gmail.com>
2025-08-18 16:19:40 -0500

    Factor out targets card

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -1,9 +1,7 @@
 import "../styles/homePage.css";
 import { useState, useEffect } from "react";
 import apiManager from "../utils/apiManager.js";
-import waldoImg from "../assets/waldo.jpg";
-import wilmaImg from "../assets/wilma.jpg";
-import wizardImg from "../assets/wizard.jpg";
+import TargetsCard from "./targetsCard.jsx";
 import ImageCard from "./imageCard.jsx";
 
 
@@ -46,20 +44,7 @@ function HomePage() {
             Try to find Waldo, Wilma, 
             and the Wizard as fast as you can!
         </p>
-        <div className="targets">
-            <div className="target-img-card">
-                <img src={waldoImg} alt="waldo" />
-                <p className="target-name">Waldo</p>
-            </div>
-            <div className="target-img-card">
-                <img src={wilmaImg} alt="wilma" />
-                <p className="target-name">Wilma</p>
-            </div>
-            <div className="target-img-card">
-                <img src={wizardImg} alt="wizard" />
-                <p className="target-name wizard-name">Wizard</p>
-            </div>
-        </div>
+        <TargetsCard />
         <p className="settings-title">Choose a Setting</p>
         <div className="image-choices">
             {imgCards}

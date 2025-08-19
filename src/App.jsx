cd422832cc0efff52b215395687eaf517fc89dcb
samuel <samawyck@gmail.com>
2025-08-19 11:07:26 -0500

    Add errorsRef to outlet context

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import './App.css';
 import Header from "./components/header.jsx";
 import Footer from './components/footer.jsx';
 import { Outlet } from 'react-router-dom';
+import { useRef } from 'react';
 
 
 
 function App() {
+    const errorsRef = useRef(null);
+
     return (
         <>
         <Header />
-        <Outlet />
+        <Outlet context={errorsRef} />
         <Footer />
         </>
     );
